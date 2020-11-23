1fda48fd772f01910474bc85322b773941cce728
Yangyu Chen <cyy@cyyself.name>
2020-11-23 18:58:21 +0800

    fix url route

--- src/main/java/cn/endereye/framework/web/WebServlet.java
+++ src/main/java/cn/endereye/framework/web/WebServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@javax.servlet.annotation.WebServlet
+@javax.servlet.annotation.WebServlet(urlPatterns = "/")
 public class WebServlet extends HttpServlet {
     private final WebManager manager = WebManager.getInstance();
 
